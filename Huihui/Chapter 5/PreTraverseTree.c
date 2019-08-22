77fb0badfaa509f0a416348455c30f45c7d8e210
Chandler Lu <chandler.lu1997@gmail.com>
2019-08-23 00:07:15 +0800

    Add PreTraverseTree and some faults

---
+++ Huihui/Chapter 5/PreTraverseTree.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 15
+#define TRUE 1
+#define FALSE 0
+
+typedef struct TreeNode {
+  char data;
+  struct TreeNode *lchild, *rchild;
+} TreeNode;
+
+typedef struct SqStack {
+  TreeNode data[MAXSIZE];
+  int top;
+} SqStack;
+
+TreeNode *BuildTree();
+void PreTraverseTree(TreeNode *);
+
+int main(int argc, char *argv[]) {
+  TreeNode *treeRoot = BuildTree();
+  PreTraverseTree(treeRoot);
+}
+
+SqStack *InitStack() {
+  SqStack *s = (SqStack *)malloc(sizeof(SqStack));
+  s->top = -1;
+  return s;
+}
+
+int StackPush(SqStack *s, TreeNode *node) {
+  if (s->top == MAXSIZE - 1) {
+    return FALSE;
+  }
+  s->data[++s->top] = *node;
+  return TRUE;
+}
+
+int StackPop(SqStack *s, TreeNode *p) {
+  if (s->top == -1) {
+    return FALSE;
+  }
+  *p = s->data[s->top--];
+  return TRUE;
+}
+
+int StackEmpty(SqStack *s) {
+  if (s->top == -1) {
+    return 1;
+  }
+  return 0;
+}
+
+TreeNode *InitTree(char data) {
+  TreeNode *treeNode = (TreeNode *)malloc(sizeof(TreeNode));
+  treeNode->data = data;
+  treeNode->lchild = NULL;
+  treeNode->rchild = NULL;
+  return treeNode;
+}
+
+TreeNode *BuildTree() {
+  TreeNode *treeRoot = InitTree('A');
+  treeRoot->lchild = InitTree('B');
+  treeRoot->lchild->rchild = InitTree('C');
+  treeRoot->lchild->rchild->lchild = InitTree('D');
+  treeRoot->lchild->rchild->rchild = InitTree('E');
+  treeRoot->rchild = InitTree('F');
+  treeRoot->rchild->rchild = InitTree('G');
+  treeRoot->rchild->rchild->lchild = InitTree('H');
+  return treeRoot;
+}
+
+void PreTraverseTree(TreeNode *root) {
+  SqStack *s = InitStack();
+  TreeNode *p = root;
+  if (p == NULL) {
+    return;
+  }
+  while (p || !StackEmpty(s)) {
+    if (p) {
+      StackPush(s, p);
+      printf("%c ", p->data);
+      p = p->lchild;
+    } else {
+      StackPop(s, p);
+      p = p->rchild;
+    }
+  }
+  free(s);
+}
